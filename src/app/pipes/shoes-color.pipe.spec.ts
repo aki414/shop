5ea13922c437cb9a252a87d60e880fcd4f93408c
aki414 <akremsedki21@gmail.com>
2025-01-07 03:30:57 +0100

    clean up

--- src/app/pipes/shoes-color.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ShoesColorPipe } from './shoes-color.pipe';
-
-describe('ShoesColorPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ShoesColorPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
